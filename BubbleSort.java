68cf4c7f618a9ce2d5f283337de71500c3c7f3be
andwany <37947242+andwany@users.noreply.github.com>
2018-09-14 14:00:53 -0700

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+
+public class BubbleSort implements SortingAlgorithm {
+
+	@Override
+	public void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			boolean swapped = false;
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					swapped = true;
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			if (!swapped) {
+				break;
+			}
+		}
+	}
+}
---
+++ SelectionSort.java
@@ -0,0 +1,19 @@
+
+public class SelectionSort implements SortingAlgorithm {
+
+	@Override
+	public void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[minIndex];
+			arr[minIndex] = temp;
+		}
+	}
+
+}
